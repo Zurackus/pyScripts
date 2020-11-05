4cbbb5b67a0c963e8859b074275647f52c9a1156
Tyler Konsonlas <tkonsonlas@hrgpros.com>
2020-11-05 08:48:23 -0800

    Notes

---
+++ pythonNotes.py
@@ -0,0 +1,9 @@
+#Show the installed Python Modules
+help('modules')
+
+#Install a new module with pip
+#pip Install <name of module>
+
+import getpass
+user = getpass.getuser()
+pwd = getpass.getpass('Password:')
